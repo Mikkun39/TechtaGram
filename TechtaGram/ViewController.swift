95c853625bddf0c7d135ae64fcd1d24bd7757ac2
Mizuki Takayama <miz39t@gmail.com>
2020-09-07 17:30:38 +0900

    グレースケールに画像フィルターを変更

--- TechtaGram/ViewController.swift
+++ TechtaGram/ViewController.swift
@@ -8,13 +8,113 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var cameraImageView: UIImageView!
+    
+    //画像加工するための元になる画像
+    var originalImage: UIImage!
+    
+    //画像加工するフィルターの宣言
+    var filter: CIFilter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //撮影ボタンを押した時のメソッド
+    @IBAction func takePhoto(){
+        
+        //カメラが使えるかの確認
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            //カメラを起動
+            
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            
+            picker.allowsEditing  = true
+            
+            present(picker, animated: true, completion: nil)
+        }else {
+            //カメラを使えない時はエラーをコンソールに表示
+            print("error")
+        }
+    }
+    
+    //撮影した写真を保存するためのメソッド
+    @IBAction func savePhoto(){
+        UIImageWriteToSavedPhotosAlbum(cameraImageView.image!, nil, nil, nil)
+    }
+    
+    //表示する画像フィルターを加工する時のメソッド
+    @IBAction func colorFilter(){
+        
+        let filterImage: CIImage = CIImage(image: originalImage)!
+        
+        //フィルターの設定
+        filter = CIFilter(name: "CIColorMonochrome")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        //彩度の調整
+        filter.setValue(CIColor(red: 0.75, green: 0.75, blue: 0.75), forKey: "inputColor")
+        //明度の調整
+        filter.setValue(1.0, forKey: "inputIntensity")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+        
+    }
+    
+    //カメラロールにある画像を読み込む時のメソッド
+    @IBAction func openAlbum(){
+        //カメラロールを使えるかの確認
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            //カメラロールの画像を選択して画像を表示するまでの一連の流れ
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    //snsに編集した画像を投稿したい時のメソッド
+    @IBAction func snsPhoto(){
+        
+        //投稿する時に一緒に載せるコメント
+        let shareText = "写真加工いえい"
+        
+        //投稿する画像の選択
+        let shareImage = cameraImageView.image!
+        
+        //投稿するコメントと画像の準備
+        let activityItems: [Any] = [shareText, shareImage]
+        
+        let activityViewController = UIActivityViewController(activityItems: activityItems,applicationActivities: nil)
+        
+        let excludedActivityTypes = [UIActivity.ActivityType.postToWeibo, .saveToCameraRoll, .print]
+        
+        activityViewController.excludedActivityTypes = excludedActivityTypes
+        
+        present(activityViewController, animated: true, completion: nil)
+        
+        
+    }
 
-
+    //カメラ、カメラロールを使った時に選択した画像をアプリ内に表示するためのメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        cameraImageView.image = info[.editedImage] as? UIImage
+        
+        originalImage = cameraImageView.image
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
